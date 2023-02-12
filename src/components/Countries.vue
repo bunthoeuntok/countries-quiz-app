cec5caef50c62f69ad99e8fdd17f5cefe636493c
Bunthoeun Tok <buntheoun.code@gmail.com>
2023-02-13 00:00:41 +0700

    separate search to it own component

--- src/components/Countries.vue
+++ src/components/Countries.vue
@@ -2,6 +2,7 @@
 import { computed, ref } from 'vue'
 import { useCountries } from '../composables/useCountries'
 import CountryModal from '../components/CountryModal.vue'
+import Search from '../components/Search.vue'
 import Fuse from 'fuse.js'
 
 const { countries, getNativeName, getIdd } = useCountries()
@@ -58,30 +59,7 @@ const results = computed(() => {
     <div class="flex justify-between items-center mb-6">
       <h1 class="text-3xl font-semibold">Countries List</h1>
 
-      <div
-        class="relative flex items-center border-2 bg-white border-gray-400 rounded-md"
-      >
-        <input
-          type="text"
-          v-model="search"
-          class="h-10 pl-10 py-2 rounded-md focus:outline-none focus-within:bg-gray-100"
-          placeholder="Search..."
-        />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6 left-2 absolute text-gray-400 font-bold"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-          />
-        </svg>
-      </div>
+      <Search v-model="search" />
     </div>
     <table class="w-full text-md text-left">
       <thead class="text-xs uppercase bg-gray-50">
---
+++ src/components/Search.vue
@@ -0,0 +1,38 @@
+<script setup>
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: '',
+  },
+})
+
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div
+    class="relative flex items-center border-2 bg-white border-gray-400 rounded-md"
+  >
+    <input
+      type="text"
+      :value="props.modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      class="h-10 pl-10 py-2 rounded-md focus:outline-none focus-within:bg-gray-100"
+      placeholder="Search..."
+    />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6 left-2 absolute text-gray-400 font-bold"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+      />
+    </svg>
+  </div>
+</template>
