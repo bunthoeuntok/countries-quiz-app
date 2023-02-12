10f31c1c4b93be58a6dcdf9b79888ad9e0bfeace
Bunthoeun Tok <buntheoun.code@gmail.com>
2023-02-12 20:55:24 +0700

    style search input

--- src/components/Countries.vue
+++ src/components/Countries.vue
@@ -60,12 +60,37 @@ const results = computed(() => {
 </script>
 
 <template>
-  <h1>Countries List</h1>
-  <input type="text" v-model="search" /> <br />
+  <div class="container mx-auto font-futura antialiased py-16 text-gray-700">
+    <div class="flex justify-between items-center mb-6">
+      <h1 class="text-3xl font-semibold">Countries List</h1>
 
-  <div class="container mx-auto font-futura antialiased py-16">
-    <table class="w-full text-md text-left text-gray-600">
-      <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+      <div
+        class="relative flex items-center border-2 bg-white border-gray-400 rounded-md"
+      >
+        <input
+          type="text"
+          v-model="search"
+          class="h-10 pl-10 py-2 rounded-md focus:outline-none focus-within:bg-gray-100"
+          placeholder="Search..."
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6 left-2 absolute text-gray-400 font-bold"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+      </div>
+    </div>
+    <table class="w-full text-md text-left">
+      <thead class="text-xs uppercase bg-gray-50">
         <tr class="bg-gray-300">
           <th scope="col" class="px-6 py-3 text-left">No</th>
           <th scope="col" class="px-6 py-3 text-center">Flag</th>
