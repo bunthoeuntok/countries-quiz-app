b45f25bf61d0aaa790386d1019516add483b2235
Bunthoeun Tok <buntheoun.code@gmail.com>
2023-02-12 15:27:34 +0700

    add CountryModal.vue to view detail

---
+++ src/components/CountryModal.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+const show = ref(false)
+const props = defineProps({
+  country: {
+    type: Object,
+    reuqired: true,
+  },
+})
+
+const toggleModal = () => (show.value = !show.value)
+</script>
+
+<template>
+  <button v-if="!show" @click="toggleModal()">
+    {{ country.name.official }}
+  </button>
+  <div v-else>show country detail</div>
+</template>
